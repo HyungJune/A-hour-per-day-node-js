02aecb95e7bb2c550684501c576c6d717b079a69
HyungJune <shinhj2512@naver.com>
2016-01-27 16:03:28 +0900

    making app.js

---
+++ Chat/app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+var io = require('socket.io').listen(server);
+
+var fs = require('fs');
+var path = require('path');
+
+app.set('port', 3000);
+
+app.use(express.favicon());
+
+/* To read post body data when it receives post transmmision */
+app.use(express.bodyParser());
+
+/* route setting for static file services */
+app.use(express.static(path.join(_dirname, 'public')));
+
+/* start server */
+app.start = app.lisen = function(){
+  return server.listen.apply(server, arguments)
+}
+
+app.start(app.get('port'), function(){
+  console.log("Server start");
+});
+
+/* service file include fucntion */
+function include(file_){
+  with(global) {
+    eval(fs.readFileSync(file_) + '');
+  };
+};
+
+/* include config file */
+include(_dirname + "/config/include.js")
+
+/* include service file */
+for(var i = 0; i < servicefile.length;i++){
+  include(_dirname + "/service/" + servicefile[i]);
+}
+
+/* process all http request */
+app.all('*', function(req, res, next){
+  next();
+});
---
+++ Chat/config/include.js
@@ -0,0 +1,1 @@
+servicefile = [file1,file2,];
